fc26194305fff1d39d745f569ad9c6ceed1c9fe9
Seohyeon Jeong(Aubrey) <cadence37@naver.com>
2018-04-03 10:58:17 +0900

    Swot진행중
    swotswot

--- src/main/java/com/palette/h5/ego/controller/EgoController.java
+++ src/main/java/com/palette/h5/ego/controller/EgoController.java
@@ -1,5 +1,22 @@
 package com.palette.h5.ego.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.palette.h5.ego.dao.EgoDAO;
+
+@Controller
+@RequestMapping(value="ego")
 public class EgoController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EgoController.class);
+	
+	@Autowired
+	EgoDAO dao;
+	
+	
 
 }
--- src/main/java/com/palette/h5/ego/dao/EgoDAO.java
+++ src/main/java/com/palette/h5/ego/dao/EgoDAO.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 public class EgoDAO {
 	
 	
+	
+	
 
 	
 }
