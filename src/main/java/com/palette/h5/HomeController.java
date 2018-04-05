cd9b5395e77d017c16ed83a8f4920c81480c9900
Unknown <merotic01@gmail.com>
2018-04-05 09:30:43 +0900

    4-5 도훈
    4-5 도훈

--- src/main/java/com/palette/h5/HomeController.java
+++ src/main/java/com/palette/h5/HomeController.java
@@ -1,6 +1,9 @@
 package com.palette.h5;
 
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.Locale;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.palette.h5.util.FileService;
 
 @Controller
 public class HomeController {
@@ -32,5 +40,37 @@ public class HomeController {
 		
 		return "common/main";
 	}
+	
+	/**
+	 * 	파일업로드
+	 */
+	@ResponseBody
+	@RequestMapping(value="fileupload", method = RequestMethod.POST)
+	public String fileupload(MultipartHttpServletRequest request) {
+		
+        Iterator<String> itr =  request.getFileNames();
+        String fullpath = "";
+        
+        if(itr.hasNext()) {
+        	
+            MultipartFile mpf = request.getFile(itr.next());
+            System.out.println(request.getServletContext().getRealPath("/resources/img/user_upload"));
+            
+            try {
+                System.out.println("file length : " + mpf.getBytes().length);
+                System.out.println("file name : " + mpf.getOriginalFilename());
+        		if (!mpf.isEmpty()) {
+        			String savedfile = FileService.saveFile(mpf, request.getServletContext().getRealPath("/resources/img/user_upload"));
+        			fullpath =  "./resources/img/user_upload" +savedfile;
+        		}
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+            return fullpath;
+        } else {
+            return fullpath;
+        }
+    }
 	
 }
