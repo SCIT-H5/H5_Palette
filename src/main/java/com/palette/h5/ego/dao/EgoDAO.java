8b2beda7ac98ab80d0d080da7b2f6bdc32b97a57
Seohyeon Jeong(Aubrey) <cadence37@naver.com>
2018-04-17 16:40:42 +0900

    0440 historydao

--- src/main/java/com/palette/h5/ego/dao/EgoDAO.java
+++ src/main/java/com/palette/h5/ego/dao/EgoDAO.java
@@ -1,11 +1,14 @@
 package com.palette.h5.ego.dao;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.palette.h5.ego.vo.History;
 import com.palette.h5.vo.FileManagement;
 
 @Repository
@@ -16,7 +19,93 @@ public class EgoDAO {
 	@Autowired
 	SqlSession sqlSession;
 	
-	// history 
+	// HISTORY | 전체 데이터 가져오기
+	public ArrayList<History> historyDataAll(String hisId){
+		
+		logger.info("DAO | history 전체 데이터 가져오기 시작");
+		
+		EgoMapper mapper = sqlSession.getMapper(EgoMapper.class);
+		ArrayList<History> historyAll = null;
+		
+		try{
+			historyAll = mapper.historyDataAll(hisId);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		logger.info("DAO | history 전체 데이터 가져오기 종료");
+		return historyAll;
+	}
+		
+	// HISTORY | 연도LIST 가져오기
+	public ArrayList<String> historyYearList(String hisId){
+		
+		logger.info("DAO | history 연도List 가져오기 시작");
+		EgoMapper mapper = sqlSession.getMapper(EgoMapper.class);
+		ArrayList<String> yearList = null;
+		
+		try{
+			yearList = mapper.historyYearList(hisId);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		logger.info("DAO | history 연도List 가져오기 종료");
+		return yearList;
+	}
+		
+	// HISTORY | 데이터 추가하기
+	public int historyAddOne(History history){
+		
+		logger.info("DAO | history 데이터 추가하기 시작");
+		EgoMapper mapper = sqlSession.getMapper(EgoMapper.class);
+		int result = 0;
+		
+		try{
+			result = mapper.historyAddOne(history);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		logger.info("DAO | history 데이터 추가하기 종료");
+		return result;
+		
+	}
+		
+	// HISTORY | 데이터 수정하기
+	public int historyUpdateOne(History history){
+		
+		logger.info("DAO | history 데이터 수정하기 시작");
+		EgoMapper mapper = sqlSession.getMapper(EgoMapper.class);
+		int result = 0;
+		
+		try{
+			result = mapper.historyUpdateOne(history);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		logger.info("DAO | history 데이터 수정하기 종료");
+		return result;
+	}
+		
+	// HISTORY | 데이터 삭제하기
+	public int historyDeleteOne(History history){
+		
+		logger.info("DAO | history 데이터 삭제하기 시작");
+		EgoMapper mapper = sqlSession.getMapper(EgoMapper.class);
+		int result = 0;
+		
+		try{
+			result = mapper.historyDeleteOne(history);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		logger.info("DAO | history 데이터 삭제하기 종료");
+		return result;
+		
+	}
+	
+	
+	
+	
 	
 	
 	
